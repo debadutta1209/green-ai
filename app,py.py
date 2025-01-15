7fca7ffc4548053cc2fc575396e619c5c1e97b37
Debadutta <153663010+debadutta1209@users.noreply.github.com>
2025-01-15 10:01:51 +0530

    Add files via upload

---
+++ app,py.py
@@ -0,0 +1,8 @@
+sapling_survival_app/
+├── sapling_survival_model.h5
+├── Raw_Data/
+│   ├── post-SW/
+│   ├── Post-Planting/
+│   ├── Post-Pitting/
+├── app.py  # Streamlit application
+├── requirements.txt
